8c913a16ea4709da8c738613bd0a2a0f1f3945c5
Jordi Izquierdo <jordi.izquierdo@unblur.co>
2023-10-21 11:37:48 +0200

    Client pretty much done

--- client/main.js
+++ client/main.js
@@ -1,3 +1,4 @@
+const MAX_HISTORY_SIZE = 60;
 const socket = new WebSocket("wss://javascript.info/article/websocket/chat/ws", ["soap", "wamp"]);
 
 socket.onopen = (_) => {
@@ -5,9 +6,22 @@ socket.onopen = (_) => {
 };
 
 socket.onmessage = (event) => {
-    const messageListItemEl = document.createElement('li');
-    messageListItemEl.textContent = event.data;
-    document.getElementById('messages').prepend(messageListItemEl);
+    const newMessage = JSON.parse(event.data);
+
+    // Append new message
+    const messageContainerEl = document.createElement('div');
+    messageContainerEl.id = 'message';
+    messageContainerEl.innerHTML = `<b>> ${newMessage['username']}</b>: ${newMessage['message']}`;
+
+    const chatContainerEl = document.getElementById('chat');
+    chatContainerEl.prepend(messageContainerEl);
+
+    // Clear older messages to keep under the maximum number
+    const historyMessages = chatContainerEl.getElementsByTagName('div');
+    while (historyMessages.length > MAX_HISTORY_SIZE) {
+        const messageToBeRemoved = historyMessages[historyMessages.length - 1];
+        chatContainerEl.removeChild(messageToBeRemoved);
+    }
 };
 
 socket.onclose = (event) => {
@@ -20,9 +34,9 @@ socket.onclose = (event) => {
 
 socket.onerror = (error) => console.error(error);
 
-const formRef = document.querySelector('form');
-formRef.onsubmit = (event) => {
+document.querySelector('form').onsubmit = (event) => {
     const formData = new FormData(event.target);
     const formProps = Object.fromEntries(formData);
-    socket.send(formProps['message']);
+    const message = JSON.stringify(formProps, null, 4);
+    socket.send(message);
 }
